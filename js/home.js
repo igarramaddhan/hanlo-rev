306ea487f26fd58d63aa3132c0f369c83b38cc55
Igar Ramaddhan <igarramaddhan@gmail.com>
2018-12-05 08:58:23 +0700

    complete post(home.html)

--- js/home.js
+++ js/home.js
@@ -1,6 +1,39 @@
 let Application = {
   init: () => {
     Application.getPosts();
+    $('#add').click(event => {
+      event.preventDefault();
+      $('.ui.modal')
+        .modal({
+          onApprove: function() {
+            const content = $('#input-content').val();
+            $.ajax({
+              url: 'https://api.ynrk.tk/ppk/createPost.php',
+              type: 'post',
+              data: { content },
+              xhrFields: {
+                withCredentials: true
+              },
+              beforeSend: function(xhr) {
+                $('.ui.approve.positive.button').toggleClass('loading');
+              },
+              success: function(dataObject) {
+                Application.getPosts();
+              },
+              error: function(dataObject) {
+                console.log('failed');
+                console.log(dataObject);
+              },
+              complete: function(dataObject) {
+                $('.ui.approve.positive.button').toggleClass('loading');
+                // $('.ui.modal').toggleClass('active');
+              }
+            });
+            return false; //Return false as to not close modal dialog
+          }
+        })
+        .modal('show');
+    });
   },
 
   getPosts: () => {
@@ -18,8 +51,21 @@ let Application = {
         $('.ui.dimmer').toggleClass('active');
       },
       success: function(dataObject) {
-        console.log('success');
-        console.log(dataObject);
+        let data = [...dataObject].reverse();
+        $('.ul.list').html('');
+        data.forEach(item => {
+          $('.ul.list').append(`
+            <div class="ui card fluid">
+              <div class="content">
+                <div class="header">${item.displayName}</div>
+                <div class="meta">${item.createdAt}</div>
+                <div class="description">
+                  ${item.content}
+                </div>
+              </div>
+            </div>
+          `);
+        });
       },
       error: function(dataObject) {
         console.log('failed');
